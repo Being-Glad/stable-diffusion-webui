4dedebed1850225049458b518cb02f0a2fb81a78
Om Pandey <83627806+Being-Glad@users.noreply.github.com>
2023-04-16 13:10:53 +0530

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--api --share --precision full --no-half --use-cpu SD GFPGAN BSRGAN ESRGAN SCUNet CodeFormer --all --skip-torch-cuda-test
 
 call webui.bat
